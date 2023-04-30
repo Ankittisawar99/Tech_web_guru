2a1387e973fd6bbb1664af3bbc4d3b758c83f1f1
Ankittisawar99 <126158909+Ankittisawar99@users.noreply.github.com>
2023-04-30 21:01:06 +0530

    Add files via upload

---
+++ conn2.php
@@ -0,0 +1,16 @@
+<?php
+$insert = false;
+$update = false;
+$delete = false;
+$login=false;
+// Connect to the Database 
+$host = "localhost";
+$username = "root";
+$password = "";
+$db = "ak1";
+// error_reporting(0);
+
+// Create a connection
+$conn = mysqli_connect($host, $username, $password, $db);
+
+?>
